704c842b86fa3a4b3190ea7cd131f71076d59e8b
Mohit Mudgil <mohittt0011@gmail.com>
2026-02-15 00:29:22 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,6 @@ import app from './app.js';
 import { PORT } from './config/env.js';
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-  console.log(`📡 API available at http://localhost:${PORT}/api`);
+  console.log(` Server running on port ${PORT}`);
+  console.log(` API available at http://localhost:${PORT}/api`);
 });
